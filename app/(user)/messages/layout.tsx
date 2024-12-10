b9dc2c841c9d874d67210f6312391a6fb533764b
Adegbite Joshua Adeoye <adegbitejoshua07@gmail.com>
2024-12-10 16:41:08 +0100

    Implementing addtional functions to the messages page

--- app/(user)/messages/layout.tsx
+++ app/(user)/messages/layout.tsx
@@ -5,18 +5,72 @@ import MessagedUser from '@/components/messages/MessagedUser'
 import ReceiverMessage from '@/components/messages/ReceiverMessage'
 import SenderMessage from '@/components/messages/SenderMessage'
 import BottomNavbar from '@/components/navbar/BottomNavbar'
-import { AttachFileOutlined, CallOutlined, ChatRounded, EditOutlined, FmdGoodOutlined, MicOutlined, MoreHorizOutlined, SearchOutlined, SendOutlined, VideocamOutlined } from '@mui/icons-material'
-import { Button, IconButton, TextField } from '@mui/material'
+import { AttachFileOutlined, CallOutlined, ChatRounded, CloseOutlined, CopyAllOutlined, DeleteOutline, EditOutlined, FmdGoodOutlined, InfoOutlined, MicOutlined, MoreHorizOutlined, ReplyAllOutlined, SearchOutlined, SendOutlined, UndoOutlined, VideocamOutlined } from '@mui/icons-material'
+import { Button, IconButton, TextField, Tooltip, Fade } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'next/navigation';
 
+export const ActionButtons = ({type, isMessagesSelected}: {type?: string, isMessagesSelected?: boolean}) => {
+    if (isMessagesSelected && type == 'messages') {
+        return (
+            <div className="flex text-slate-400 gap-3 items-center">
+                <Tooltip title="Copy" arrow>
+                    <IconButton>
+                        <CopyAllOutlined fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Vioce Call" arrow>
+                    <IconButton>
+                        <DeleteOutline fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="More" arrow>
+                    <IconButton className='h-10 w-10 flex justify-center items-center border rounded-full'>
+                        <InfoOutlined fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="More" arrow>
+                    <IconButton className='h-10 w-10 flex justify-center items-center border rounded-full'>
+                        <ReplyAllOutlined fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="More" arrow>
+                    <IconButton className='h-10 w-10 flex justify-center items-center border rounded-full'>
+                        <UndoOutlined fontSize='large' />
+                    </IconButton>
+                </Tooltip>
+            </div>
+        )
+    }
 
+    return (
+        <div className="flex text-slate-400 gap-3 items-center">
+            <Tooltip title="Video Call" arrow>
+                <IconButton>
+                    <VideocamOutlined fontSize='large' />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Vioce Call" arrow>
+                <IconButton>
+                    <CallOutlined fontSize='large' />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="More" arrow>
+                <IconButton className='h-10 w-10 flex justify-center items-center border rounded-full'>
+                    <MoreHorizOutlined fontSize='large' />
+                </IconButton>
+            </Tooltip>
+        </div>
+    )
+}
 
 export default function Layout() {
     const [showMessages, setShowMessages] = useState(false);
     const [isMediumSize, setIsMediumSize] = useState(false);
+    const [isMessagesSelected, setIsMessagesSelected] = useState(false);
+    const [selectedMessages, setSelectedMessages] = useState<string[]>([]);
     const params = useParams();
 
 
@@ -35,6 +89,11 @@ export default function Layout() {
 
     };
 
+    const handleSetSelectedMessages = () => {
+        setIsMessagesSelected(!isMessagesSelected);
+        setSelectedMessages(isMessagesSelected? [...selectedMessages, '123'] : []);
+    };
+
     useEffect(() => {
         const handleResize = () => {
             const width = window.innerWidth;
@@ -43,7 +102,7 @@ export default function Layout() {
 
         const { id } = params;
         console.log(params);
-        
+
         setShowMessages(id ? true : false);
 
         handleResize();
@@ -63,10 +122,34 @@ export default function Layout() {
         }
     }, []);
 
+    const [viewProfile, setViewProfile] = useState(false);
+
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
+    const [filteredMessages, setFilteredMessages] = useState<string[]>([]);
+    const searchInputRef = useRef(null);
+
+    const toggleSearch = () => {
+        setIsSearchVisible((prev) => !prev);
+    };
+
+    const handleSearch = (event: any) => {
+        const searchText = event.target.value.toLowerCase();
+        const filtered = ['hhhh', 'jjj'].filter((msg) =>
+            msg.toLowerCase().includes(searchText)
+        );
+        setFilteredMessages(filtered);
+    };
+
+    useEffect(() => {
+        if (isSearchVisible && searchInputRef.current) {
+            // searchInputRef.current.focus();
+        }
+    }, [isSearchVisible]);
+
     return (
         <div className='basis-full md:basis-10/12 flex'>
             <div className="basis-full md:basis-2/6 border-r border-slate-200">
-                <div className='h-[10%] md:h-1/6 p-3 flex items-center justify-between'>
+                {/* <div className='h-[10%] md:h-1/6 p-3 flex items-center justify-between'>
                     <div>
                         <Button className='md:hidden text-blue-600 font-bold text-sm items-center' startIcon={
                             <Image src={'/logo-t.png'} alt='Logo' height={40} width={40} />
@@ -80,6 +163,47 @@ export default function Layout() {
                         <IconButton>
                             <SearchOutlined className='text-slate-400 mx-2' />
                         </IconButton>
+                    </div>
+                </div> */}
+                <div className='h-[10%] md:h-1/6 p-3 flex items-center justify-between relative'>
+                    <div>
+                        <Button
+                            className='md:hidden text-blue-600 font-bold text-sm flex items-center'
+                            startIcon={
+                                <Image src={'/logo-t.png'} alt='Logo' height={40} width={40} />
+                            }
+                        >
+                            BUZZCHAT
+                        </Button>
+                        <p className="text-sm md:text-xl text-blue-600">Messages</p>
+                    </div>
+                    <div className="flex items-center relative">
+                        <IconButton>
+                            <EditOutlined className='text-slate-400 mx-2' />
+                        </IconButton>
+
+                        <IconButton onClick={toggleSearch}>
+                            <SearchOutlined className='text-slate-400 mx-2' />
+                        </IconButton>
+
+                        <Fade in={isSearchVisible}>
+                                <div className="absolute right-0 top-full mt-2 w-64">
+                                    <TextField
+                                        inputRef={searchInputRef}
+                                        variant="outlined"
+                                        size="small"
+                                        placeholder="Search messages..."
+                                        onChange={handleSearch}
+                                        autoComplete="off"
+                                        fullWidth
+                                        sx={{
+                                            backgroundColor: 'white',
+                                            borderRadius: '4px',
+                                            boxShadow: 1,
+                                        }}
+                                    />
+                                </div>
+                        </Fade>
                     </div>
                 </div>
                 <hr />
@@ -114,87 +238,102 @@ export default function Layout() {
                 <BottomNavbar isMediumSize={isMediumSize} />
             </div>
             <div className={`${showMessages && isMediumSize ? 'block absolute top-0 left-0 z-10 w-full h-full' : 'hidden'} md:block md:basis-4/6 bg-slate-200`}>
-                <div className='h-1/6 p-3 bg-white flex items-center justify-between'>
-                    <div className='flex gap-3'>
-                        <Link href={'/messages'} className='flex justify-center items-center'>
-                            <img src={`/logo.png`} alt={`Another friend`} className='h-12 w-12 aspect-square rounded-full' />
-                        </Link>
-                        <div className=''>
-                            <p className="font-bold">Chinedu Oyenre</p>
-                            <p className="text-green-400 text-sm">Typing...</p>
+                <div className='w-full h-full relative'>
+                    <div className='h-1/6 p-3 bg-white flex items-center justify-between'>
+                        <div className='flex gap-3'>
+                            <Link href={'/messages'} className='flex justify-center items-center'>
+                                <img src={`/logo.png`} alt={`Another friend`} className='h-12 w-12 aspect-square rounded-full' />
+                            </Link>
+                            <div onClick={() => setViewProfile(!viewProfile)} className='cursor-pointer'>
+                                <p className="font-bold">Chinedu Oyenre</p>
+                                <p className="text-green-400 text-sm">Typing...</p>
+                            </div>
+                        </div>
+                        <ActionButtons type={'messages'} isMessagesSelected={isMessagesSelected} />
+                    </div>
+                    <hr />
+                    <div className="h-5/6 p-3 relative">
+                        {/* Date Label */}
+                        <div className='h-[calc(100%-56px)] overflow-y-auto scrollbar-hidden'>
+                            <div className="absolute flex w-full">
+                                <span className="p-1 text-xs bg-white text-black rounded-md mx-auto">
+                                    Today, Dec 30
+                                </span>
+                            </div>
+
+                            {/* Messages */}
+                            <div>
+                                <ReceiverMessage />
+                                <ReceiverMessage />
+                                <ReceiverMessage />
+                                <SenderMessage handleSetSelectedMessages={handleSetSelectedMessages} isMessagesSelected={isMessagesSelected} />
+                                <ReceiverMessage />
+                                <SenderMessage handleSetSelectedMessages={handleSetSelectedMessages} isMessagesSelected={isMessagesSelected} />
+                                <SenderMessage handleSetSelectedMessages={handleSetSelectedMessages} isMessagesSelected={isMessagesSelected} />
+                                <SenderMessage handleSetSelectedMessages={handleSetSelectedMessages} isMessagesSelected={isMessagesSelected} />
+                                <span ref={lastMessageRef} className="h-0"></span>
+                            </div>
+                        </div>
+
+                        <AttachmentPopover
+                            anchorEl={anchorEl}
+                            open={Boolean(anchorEl)}
+                            onClose={handlePopoverClose}
+                            onSelectAttachment={handleSelectAttachment}
+                        />
+                        {/* Sticky Input Field */}
+                        <div className="w-full h-14">
+                            <div className="flex items-center bg-slate-300 rounded-full w-full h-14 px-4">
+                                <Tooltip title="Attach files">
+                                    <IconButton onClick={handleAttachmentClick}>
+                                        <AttachFileOutlined />
+                                    </IconButton>
+                                </Tooltip>
+
+                                <TextField
+                                    variant="outlined"
+                                    placeholder="Type a message..."
+                                    size="small"
+                                    className='w-5/6'
+                                    sx={{
+                                        '& .MuiOutlinedInput-root': {
+                                            backgroundColor: 'white',
+                                            borderRadius: '20px',
+                                        },
+                                    }}
+                                />
+
+                                <Tooltip title="Voice note">
+                                    <IconButton>
+                                        <MicOutlined />
+                                    </IconButton>
+                                </Tooltip>
+
+                                <Tooltip title="Send">
+                                    <IconButton>
+                                        <SendOutlined />
+                                    </IconButton>
+                                </Tooltip>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex text-slate-400 gap-3 items-center">
-                        <IconButton>
-                            <VideocamOutlined fontSize='large' />
-                        </IconButton>
-                        <IconButton>
-                            <CallOutlined fontSize='large' />
-                        </IconButton>
-                        <IconButton className='h-10 w-10 flex justify-center items-center border rounded-full'>
-                            <MoreHorizOutlined fontSize='large' />
-                        </IconButton>
-                    </div>
-                </div>
-                <hr />
-                <div className="h-5/6 p-3 relative">
-                    {/* Date Label */}
-                    <div className='h-[calc(100%-56px)] overflow-y-auto scrollbar-hidden'>
-                        <div className="absolute flex w-full">
-                            <span className="p-1 text-xs bg-white text-black rounded-md mx-auto">
-                                Today, Dec 30
-                            </span>
-                        </div>
-
-                        {/* Messages */}
-                        <div>
-                            <ReceiverMessage />
-                            <ReceiverMessage />
-                            <ReceiverMessage />
-                            <SenderMessage />
-                            <ReceiverMessage />
-                            <SenderMessage />
-                            <SenderMessage />
-                            <SenderMessage />
-                            <span ref={lastMessageRef} className="h-0"></span>
-                        </div>
-                    </div>
-
-                    <AttachmentPopover
-                        anchorEl={anchorEl}
-                        open={Boolean(anchorEl)}
-                        onClose={handlePopoverClose}
-                        onSelectAttachment={handleSelectAttachment}
-                    />
-                    {/* Sticky Input Field */}
-                    <div className="w-full h-14">
-                        <div className="flex items-center bg-slate-300 rounded-full w-full h-14 px-4">
-                            <IconButton onClick={handleAttachmentClick}>
-                                <AttachFileOutlined />
-                            </IconButton>
-
-                            <TextField
-                                variant="outlined"
-                                placeholder="Type a message..."
-                                size="small"
-                                className='w-5/6'
-                                sx={{
-                                    '& .MuiOutlinedInput-root': {
-                                        backgroundColor: 'white',
-                                        borderRadius: '20px',
-                                    },
-                                }}
-                            />
-
-                            <IconButton>
-                                <MicOutlined />
-                            </IconButton>
-
-                            <IconButton>
-                                <SendOutlined />
-                            </IconButton>
-                        </div>
-                    </div>
+                    {viewProfile &&
+                        <div className="absolute top-0 left-0 w-full h-full p-3 bg-slate-200">
+                            <Tooltip title="Close" arrow>
+                                <IconButton onClick={() => setViewProfile(false)}>
+                                    <CloseOutlined />
+                                </IconButton>
+                            </Tooltip>
+                            <div className="flex flex-col items-center">
+                                <img src="/logo.png" className="h-24 w-24 relative rounded-full border-2 border-blue-600" />
+                                <div className="flex gap-2 items-center">
+                                    <div className="h-2 w-2 bg-green-500 rounded-full" />
+                                    <p className="font-bold text-2xl">Adegbite Joshua</p>
+                                </div>
+                                <small className="text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, quo?...</small>
+                                <ActionButtons />
+                            </div>
+                        </div>}
                 </div>
             </div>
         </div>
--- components/messages/SenderMessage.tsx
+++ components/messages/SenderMessage.tsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function SenderMessage() {
+export default function SenderMessage({isMessagesSelected, handleSetSelectedMessages}: {isMessagesSelected: boolean, handleSetSelectedMessages: ()=>any}){
+  const [isSelected, setIsSelected] = useState(false)
+  let pressTimer: any = null;
+
+  const handleLongPressStart = () => {
+    // Start the timer
+    pressTimer = setTimeout(() => {
+      console.log("Long Press Triggered!");
+      setIsSelected(prev => !prev);
+    }, 500); // 500ms threshold
+  };
+
+  const handleLongPressEnd = () => {
+    // Clear the timer
+    clearTimeout(pressTimer);
+  };
+
   return (
-    <div className='flex gap-3 ms-auto max-w-[300px] my-2 md:w-auto'>
-        <div>
-            <div className='flex items-end justify-end gap-3'>
-                <small className='text-slate-400'>05:01 PM</small>
-                <p className="font-bold">You</p>
-            </div>
-            <p className='bg-blue-600 rounded-xl rounded-tr-none p-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos harum accusantium mollitia natus! Iste modi aperiam explicabo cumque aspernatur voluptatem.</p>
+    <div
+      className='flex gap-3 ms-auto max-w-[300px] my-2 md:w-auto'
+      onMouseDown={handleLongPressStart}
+      onMouseUp={handleLongPressEnd}
+      onMouseLeave={handleLongPressEnd}
+      onTouchStart={handleLongPressStart}
+      onTouchEnd={handleLongPressEnd}>
+      <div>
+        <div className='flex items-end justify-end gap-3'>
+          <small className='text-slate-400'>05:01 PM</small>
+          <p className="font-bold">You</p>
         </div>
-        <img src="/logo.png" className='h-12 w-12 rounded-full' alt="" />
+        <p className={`${isSelected ? 'bg-yellow-300' : 'bg-blue-600'} rounded-xl rounded-tr-none p-2`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos harum accusantium mollitia natus! Iste modi aperiam explicabo cumque aspernatur voluptatem.</p>
+      </div>
+      <img src="/logo.png" className='h-12 w-12 rounded-full' alt="" />
     </div>
   )
 }
